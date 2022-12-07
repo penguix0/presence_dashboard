105f90953d0e6f11c66c8a1f381113c68459abc9
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-12-07 18:34:08 +0100

    Added nginx to the setup script

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,8 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"
+bind = "unix:dashboard.sock"
 ## Using threads instead of workers because csrf gets messed up otherwise
 workers = 1
 threads = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "dashboard:app"
+umask = "007"
--- setup.sh
+++ setup.sh
@@ -8,16 +8,32 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 deactivate
 
+# Make the startup file executable by the owner
+sudo chmod 744 /var/flaskapp/simpleapp/start.sh
+
 #Supervisor to start and control the server
 sudo apt-get install supervisor -y
 
 sudo chown -R www-data:www-data $PWD
 
-
 # Replace variables
-sudo sed 's/VAR1/$PWD/g' ./deployment_files/app.conf
+sudo sed 's/VAR1/${PWD}/g' ./deployment_files/app.conf
 
 # Copy file over
 sudo cp ./deployment_files/app.conf /etc/supervisor/conf.d/
 
 sudo systemctl restart supervisor.service
+
+# Setup nginx
+
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo cp ./deployment_files/app /etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+
+sudo nginx -t
+
+sudo nginx -s reload
+
+sudo systemctl restart nginx
