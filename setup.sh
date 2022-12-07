49dc9bbab081c65a4a6704385e3d31f419b4d649
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-12-07 11:33:02 +0100

    expanding setup script to feature supervisor configuration

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 deactivate
+
+#Supervisor to start and control the server
+sudo apt-get install supervisor -y
+
+sudo chown -R www-data:www-data $PWD
+
+sudo nano /etc/supervisor/conf.d/simpleapp.conf
+
+sudo systemctl restart supervisor.service
